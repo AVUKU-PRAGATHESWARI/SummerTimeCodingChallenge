f110cdce6217022c7a01487889eb98a36433128b
Pragatheswari Avuku <pragatheswari2004@gmail.com>
2023-05-16 07:55:53 +0530

    DaySeventeen

---
+++ Square Root of a number30.py
@@ -0,0 +1,9 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def sqrtN(n):
+	return int(sqrt(n))
+
+# https://www.codingninjas.com/codestudio/problems/square-root-integral_893351?leftPanelTab=0
